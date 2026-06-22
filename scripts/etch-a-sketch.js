b34a7c94ab9bd827d4acd35e7b7808580947a2c7
EthanMeli <etme6835@colorado.edu>
2026-06-22 20:39:22 +0200

    Create initial grid layout

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+// create a row container for every row
+// create a square in col loop
+// apply square class to each new element
+// append square to row container
+// append row container to container
+function initializeGrid(dims) {
+  const container = document.querySelector(".container");
+  container.childNodes.forEach(element => element.remove());
+
+  for (let i = 0; i < dims; i++) {
+    const row = document.createElement("div");
+    for (let j = 0; j < dims; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+    }
+    container.appendChild(row);
+  }
+}
+
+initializeGrid(16);
