540f53f34774a73ba75c988a2cb4db4acab0e594
EthanMeli <etme6835@colorado.edu>
2026-06-22 21:11:46 +0200

    Create resizing feature

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,11 +1,21 @@
-// create a row container for every row
-// create a square in col loop
-// apply square class to each new element
-// append square to row container
-// append row container to container
+const container = document.querySelector(".container");
+
+const createNewBtn = document.querySelector(".create-new");
+createNewBtn.addEventListener("click", () => {
+  const dimensions = Number(prompt("What dimensions would you like the new sketch to be?", 16));
+  if (dimensions === "NaN") {
+    alert("Please input a number!");
+  } else if (dimensions > 100) {
+    alert("This number is too large! Please enter an integer between 1-100.");
+  } else if (Math.floor(dimensions) !== dimensions) {
+    alert("Please enter an integer.");
+  } else {
+    initializeGrid(dimensions);
+  }
+})
+
 function initializeGrid(dims) {
-  const container = document.querySelector(".container");
-  container.childNodes.forEach(element => element.remove());
+  container.innerHTML = '';
 
   for (let i = 0; i < dims; i++) {
     const row = document.createElement("div");
